79f696eeb14cf00399f0730fd3c0132eec116c8d
Chanwon-Seo <scwonn60@gmail.com>
2024-07-11 10:25:37 +0900

    🐛 Fix(유저): 불필요 필드 제거

--- src/main/java/com/wolf/workflow/common/exception/global/GlobalNotFoundException.java
+++ src/main/java/com/wolf/workflow/common/exception/global/GlobalNotFoundException.java
@@ -2,13 +2,9 @@ package com.wolf.workflow.common.exception.global;
 
 import lombok.Getter;
 
-@Getter
 public class GlobalNotFoundException extends RuntimeException {
-
-    private final String errorMessage;
 
     public GlobalNotFoundException(String message) {
         super(message);
-        this.errorMessage = message;
     }
 }
